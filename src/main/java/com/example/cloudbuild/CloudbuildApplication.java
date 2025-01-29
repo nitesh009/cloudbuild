2bd7c915a4927334962f477fefbb666f80d13cc9
nitesh009 <niteshgnit3@gmail.com>
2025-01-29 19:46:07 +0530

    first commit

---
+++ src/main/java/com/example/cloudbuild/CloudbuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudbuild/CloudbuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
